838b0ce183ab3dfb0b5d83007cd751b036246cea
Douglas Gonçalo <sdouglas968@gmail.com>
2023-11-14 18:37:45 -0300

    ok

--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -8,22 +8,9 @@ const cartReducer = (state = initialState, action) =>{
 switch (action.type){
 case CartActionTypes.REMOVE_PRODUCT:
     return{
-        ...state,
-        products: state.products.filter(product => product.id !== action.payload)
+        
     }
-case CartActionTypes.INCREASE_PRODUCT_QUANTITY: 
-return{
-    ...state,
-    products: state.products.map((product) => product.id === action.payload ? { ...product, quantity: product.quantity + 1} : product)
-}
-case CartActionTypes.DECREASE_PRODUCT_QUANTITY:
-    return{
-        ...state,
-    products: state.products.map((product) => product.id === action.payload ? { ...product, quantity: product.quantity - 1} : product)
-    .filter((product) => product.quantity > 0)
-    }
-    default:
-        return state
+   
 } 
 }
 export default cartReducer
